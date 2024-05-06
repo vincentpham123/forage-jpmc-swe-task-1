16896bdfb83780584f225288b701a98bc7efb003
vincentpham123 <vincentpham32@gmail.com>
2024-05-06 16:11:39 -0700

    added two more tests Test #1 added a unit test that will check if getRatio function will return the correct value
    text #2
    added a unit test to check if None is returned if price_b is Zero

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -21,6 +21,32 @@ class ClientTest(unittest.TestCase):
 
   """ ------------ Add more unit tests ------------ """
 
-  
+  def test_getRatio_calculateRatio(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    stock, bid_pricea, ask_pricea, price_a = getDataPoint(quotes[0])
+    stock, bid_priceb, ask_priceb, price_b = getDataPoint(quotes[1])
+
+    ratio = price_a / price_b 
+
+    self.assertEqual(getRatio(price_a, price_b), ratio)
+
+  def test_getRatio_calculateRatioWhenBidEqualsZero(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    stock_a, bid_pricea, ask_pricea, price_a= getDataPoint(quotes[0])
+    stock_b, bid_priceb, ask_priceb, price_b= getDataPoint(quotes[1])
+
+    self.assertEqual(0,price_b)
+
+    self.assertEqual(getRatio(price_a, price_b), None)
+
+    
+
 if __name__ == '__main__':
     unittest.main()
